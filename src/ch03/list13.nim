923a5c1af366181de63007b395dbc9f4c1a42bc5
mikan <yutakakato@icloud.com>
2018-04-02 20:35:20 +0900

    add 3-13

---
+++ src/ch03/list13.nim
@@ -0,0 +1,20 @@
+import asyncdispatch, asyncnet
+
+type
+    Client = ref object
+        socket: AsyncSocket
+        netAddr: string
+        id: int
+        connected: bool
+    
+    Server = ref object
+        socket: AsyncSocket
+        clients: seq[Client]
+
+var sv = new Server
+echo(sv.clients.len)
+
+#[
+$ nim c -r lsit13
+0
+]#
